f58778ee7f2418720b19c254b0a9ae0c723d97db
fridego <931035186@qq.com>
2018-06-28 03:35:37 +0800

    rm-console

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,6 +1,5 @@
 import Home, {Icon as HomeIcon} from './Home';
 import DashBoard, {Icon as DashBoardIcon} from './DashBoard';
-console.log(DashBoard);
 
 export const modules = {Home, DashBoard};
 export const icons = {HomeIcon, DashBoardIcon};
